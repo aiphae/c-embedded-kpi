fcf46bacb504d280acefb406756300632483e6aa
aiphae <vl.diachenkoo@gmail.com>
2025-09-15 14:54:14 +0300

    Adjustment

--- pract/pract_string.c
+++ pract/pract_string.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char *transform_string(char *str) {
+void transform_string(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if ((i + 1) % 2 == 1) {
             str[i] = toupper((unsigned char) str[i]);
@@ -10,11 +10,11 @@ char *transform_string(char *str) {
             str[i] = tolower((unsigned char) str[i]);
         }
     }
-    return str;
 }
 
 int main() {
     char str[] = "Hello, world!";
     printf("Original string: %s\n", str);
-    printf("Transformed string: %s\n", transform_string(str));
+    transform_string(str);
+    printf("Transformed string: %s\n", str);
 }
