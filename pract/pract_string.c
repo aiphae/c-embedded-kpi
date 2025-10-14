fd38c77170187835ef705cfc32973b34653d7224
aiphae <vl.diachenkoo@gmail.com>
2025-10-14 23:58:02 +0300

    Adjust

--- pract/pract_string.c
+++ pract/pract_string.c
@@ -2,7 +2,14 @@
 #include <ctype.h>
 
 void transform_string(char *str) {
+    if (str == NULL) {
+        return;
+    }
+
     for (int i = 0; str[i] != '\0'; i++) {
+        if (!isalpha((unsigned char) str[i])) {
+            continue;
+        }
         if ((i + 1) % 2 == 1) {
             str[i] = toupper((unsigned char) str[i]);
         }
@@ -16,5 +23,15 @@ int main() {
     char str[] = "Hello, world!";
     printf("Original string: %s\n", str);
     transform_string(str);
-    printf("Transformed string: %s\n", str);
+    printf("Transformed string: %s\n\n", str);
+
+    char str2[] = "Hello, world! 123456!@#$%^&*()_+";
+    printf("Original string: %s\n", str2);
+    transform_string(str2);
+    printf("Transformed string: %s\n\n", str2);
+
+    char *str3 = NULL;
+    printf("Original string: %s\n", str3);
+    transform_string(str3);
+    printf("Transformed string: %s\n", str3);
 }
